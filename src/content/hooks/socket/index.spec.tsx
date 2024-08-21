a3e868ae85342b1cf63c2e22bf5bf6f931df2ba9
jingjiezhou <jingjiezhou@tencent.com>
2024-08-21 20:09:35 +0800

    test for socket

---
+++ src/content/hooks/socket/index.spec.tsx
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react'
+import { useSocket } from '.'
+import { Server } from 'mock-socket';
+
+const socketTestCase = async () => {
+    const fakeURL = 'ws://localhost:8080';
+    const mockServer = new Server(fakeURL);
+    mockServer.on('connection', socket => {
+        console.log('mock server is connected')
+    });
+    const {result}=renderHook(() => { return useSocket(fakeURL) })
+    const[_,isReady]=result.current
+    setTimeout(()=>{
+        expect(isReady).toBe(true)
+    },1000)
+    
+}
+test("useSocket hook",socketTestCase)
