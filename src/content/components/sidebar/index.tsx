56c07298be011cfdc68e5878135fa533295af959
jingjiezhou <jingjiezhou@tencent.com>
2024-08-23 14:44:23 +0800

    side bar style changed

--- src/content/components/sidebar/index.tsx
+++ src/content/components/sidebar/index.tsx
@@ -1,4 +1,25 @@
 import "./index.scss";
+import { Menu, MenuProps } from "antd";
+import { MessageOutlined, UserOutlined } from "@ant-design/icons";
+type MenuItem = Required<MenuProps>["items"][number];
+const items: MenuItem[] = [
+	{
+		key: "chatroom",
+		icon: <MessageOutlined />,
+	},
+	{
+		key: "myaccount",
+		icon: <UserOutlined />,
+	},
+];
 export default function Sidebar() {
-	return <div className="sidebar">this is the side bar</div>;
+	return (
+		<div className="sidebar">
+			<Menu
+				items={items}
+				defaultSelectedKeys={["chatroom"]}
+				className="sideMenu"
+			></Menu>
+		</div>
+	);
 }
