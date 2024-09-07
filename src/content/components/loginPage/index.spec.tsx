38adac6f10a2a950ffe4c6861b31543c7eb375e3
joe <musketeerdt@gmail.com>
2024-09-07 14:52:11 +0800

    email check unit test

--- src/content/components/loginPage/index.spec.tsx
+++ src/content/components/loginPage/index.spec.tsx
@@ -10,21 +10,23 @@ import {
 } from "@testing-library/react";
 import { apolloMocks } from "./test/mocks";
 import { store } from "@/content/store";
-
 describe("loginPage", () => {
-	const router = createMemoryRouter(routes, {
-		initialEntries: ["/login"],
-		initialIndex: 0,
-	});
-	const renderObj = (
-		<Provider store={store}>
-			<MockedProvider mocks={apolloMocks}>
-				<RouterProvider router={router}></RouterProvider>
-			</MockedProvider>
-		</Provider>
-	);
+	function generateRenderObj() {
+		const router = createMemoryRouter(routes, {
+			initialEntries: ["/login"],
+			initialIndex: 0,
+		});
+		const renderObj = (
+			<Provider store={store}>
+				<MockedProvider mocks={apolloMocks}>
+					<RouterProvider router={router}></RouterProvider>
+				</MockedProvider>
+			</Provider>
+		);
+		return renderObj;
+	}
 	test("switch between login and register", async () => {
-		render(renderObj);
+		render(generateRenderObj());
 		let toggler = screen.getByText("Sign Up");
 		await userEvent.click(toggler);
 		expect(screen.getByText("Register")).toBeTruthy();
@@ -33,11 +35,11 @@ describe("loginPage", () => {
 		expect(screen.getByText("Login")).toBeTruthy();
 	});
 	test("Default display login form", async () => {
-		render(renderObj);
+		render(generateRenderObj());
 		expect(screen.getByText("Login")).toBeTruthy();
 	});
 	test("Confirm Password", async () => {
-		render(renderObj);
+		render(generateRenderObj());
 		let toggler = screen.getByText("Sign Up");
 		await userEvent.click(toggler);
 		let pwd: HTMLInputElement = screen.getByLabelText("Password");
@@ -62,5 +64,37 @@ describe("loginPage", () => {
 				"The new password that you entered do not match!",
 			),
 		);
+	});
+	test("Email Check", async () => {
+		const ERR_TIP_TEXT = "The input is not valid E-mail!";
+		render(generateRenderObj());
+		let toggler = screen.getByText("Sign Up");
+		userEvent.click(toggler);
+		let email: HTMLInputElement = screen.getByLabelText("Email");
+		async function emailAssert(content: string, res: boolean) {
+			userEvent.click(email);
+			userEvent.keyboard(content);
+			expect(email.value).toBe(content);
+			if (res) {
+				expect(screen.queryByText(ERR_TIP_TEXT)).toBe(null);
+			} else {
+				const tmp = await screen.findByText(ERR_TIP_TEXT);
+				expect(tmp).toBeTruthy();
+			}
+			userEvent.clear(email);
+			if (screen.queryByText(ERR_TIP_TEXT)) {
+				await waitForElementToBeRemoved(() =>
+					screen.queryByText(ERR_TIP_TEXT),
+				);
+			}
+		}
+		await emailAssert("jingjiezhou", false);
+		await emailAssert("@qq.com", false);
+		await emailAssert("jingjie@qq.com@163.com", false);
+		await emailAssert("1dfweg,hbjh@qq.com", false);
+		await emailAssert("jing,jie@qq.com", false);
+		await emailAssert("jingjie@q,q.com", false);
+		await emailAssert("jingjie@qq.com", true);
+		await screen.findByText("Please input your Username!");
 	});
 });
