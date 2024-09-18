cfd68741a152e41338595209fbd7f091248ebac4
joe <musketeerdt@gmail.com>
2024-09-18 08:13:28 +0800

    edit-profile avatar section placed

--- src/content/components/options/editProfile/index.tsx
+++ src/content/components/options/editProfile/index.tsx
@@ -1,3 +1,21 @@
+import { Avatar, Button } from "antd";
+import "./index.scss";
+import { UserOutlined } from "@ant-design/icons";
 export default function EditProfile() {
-	return <div className="edit-profile"></div>;
+	return (
+		<div className="edit-profile">
+			<div className="avatar-section">
+				<Avatar
+					icon={<UserOutlined />}
+					className="avatar"
+					size={56}
+				/>
+				<div className="info">
+					<span className="username">username</span>
+					<span className="email">email</span>
+				</div>
+				<Button type="primary">Change photo</Button>
+			</div>
+		</div>
+	);
 }
