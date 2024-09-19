e210e3802018b9972a95a563d9a577e2fd18528e
joe <musketeerdt@gmail.com>
2024-09-19 17:18:09 +0800

    use formik for form management

--- src/content/components/options/editProfile/index.tsx
+++ src/content/components/options/editProfile/index.tsx
@@ -1,10 +1,12 @@
-import { Avatar, Button, Input, Select } from "antd";
+import { Avatar, Button, Input } from "antd";
+import { Select } from "formik-antd";
 import "./index.scss";
 import { UserOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { Gender } from "@/common/gql/graphql";
 import { useAppSelector } from "@/content/hooks/store";
 import { selectId } from "@/content/store/userSlice";
+import { Formik, FormikProps, Form } from "formik";
 const { TextArea } = Input;
 type updateField = {
 	id: string;
@@ -14,111 +16,99 @@ type updateField = {
 	gender: Gender | null;
 };
 export default function EditProfile() {
-	const [changed, setChanged] = useState(false);
 	const myid = useAppSelector(selectId);
-	const [formState, setFormState] = useState<updateField>({
+	const initVals: updateField = {
 		id: myid,
 		name: "",
 		website: "",
 		bio: "",
 		gender: null,
-	});
+	};
 	return (
-		<div className="edit-profile">
-			<div className="avatar-section">
-				<Avatar
-					icon={<UserOutlined />}
-					className="avatar"
-					size={56}
-				/>
-				<div className="info">
-					<span className="username">username</span>
-					<span className="email">email</span>
-				</div>
-				<Button type="primary">Change photo</Button>
-			</div>
-			<div className="section username-section">
-				<div className="sub-content-title">Username</div>
-				<Input
-					showCount
-					maxLength={20}
-					style={{ height: 40 }}
-					value={formState.name}
-					onInput={(e) => {
-						setChanged(true);
-						setFormState(
-							Object.assign({}, formState, {
-								name: e.currentTarget.value,
-							}),
-						);
-					}}
-				/>
-			</div>
-			<div className="website-section section">
-				<div className="sub-content-title">Website</div>
-				<Input
-					style={{ height: 40 }}
-					value={formState.website}
-					onInput={(e) => {
-						setChanged(true);
-						setFormState(
-							Object.assign({}, formState, {
-								website: e.currentTarget.value,
-							}),
-						);
-					}}
-				/>
-			</div>
-			<div className="bio-section section">
-				<div className="sub-content-title">Bio</div>
-				<TextArea
-					showCount
-					maxLength={150}
-					style={{ height: 60, resize: "none" }}
-					value={formState.bio}
-					onInput={(e) => {
-						setChanged(true);
-						setFormState(
-							Object.assign({}, formState, {
-								bio: e.currentTarget.value,
-							}),
-						);
-					}}
-				/>
-			</div>
-			<div className="gender-section section">
-				<div className="sub-content-title">Gender</div>
-				<Select
-					className="gender-selection"
-					options={[
-						{ value: "male", label: "Male" },
-						{ value: "female", label: "Female" },
-						{ value: "nottosay", label: "Prefer not to say" },
-					]}
-					value={formState.gender}
-					onChange={(e) => {
-						setChanged(true);
-						setFormState(
-							Object.assign({}, formState, {
-								gender: e,
-							}),
-						);
-					}}
-				/>
-				<div className="caption">
-					This won’t be part of your public profile.
-				</div>
-			</div>
-			<div className="section submit-section">
-				<Button
-					type="primary"
-					disabled={!changed}
-					className="submit"
-				>
-					Submit
-				</Button>
-				<Button disabled={!changed}>Undo</Button>
-			</div>
-		</div>
+		<Formik
+			initialValues={initVals}
+			onSubmit={(values) => {
+				console.log(values);
+			}}
+		>
+			{(props: FormikProps<updateField>) => (
+				<Form className="edit-profile">
+					<div className="avatar-section">
+						<Avatar
+							icon={<UserOutlined />}
+							className="avatar"
+							size={56}
+						/>
+						<div className="info">
+							<span className="username">username</span>
+							<span className="email">email</span>
+						</div>
+						<Button type="primary">Change photo</Button>
+					</div>
+					<div className="section username-section">
+						<div className="sub-content-title">Username</div>
+						<Input
+							name="name"
+							showCount
+							maxLength={20}
+							style={{ height: 40 }}
+							value={props.values.name}
+							onInput={props.handleChange}
+						/>
+					</div>
+					<div className="website-section section">
+						<div className="sub-content-title">Website</div>
+						<Input
+							name="website"
+							style={{ height: 40 }}
+							value={props.values.website}
+							onInput={props.handleChange}
+						/>
+					</div>
+					<div className="bio-section section">
+						<div className="sub-content-title">Bio</div>
+						<TextArea
+							name="bio"
+							showCount
+							maxLength={150}
+							style={{ height: 60, resize: "none" }}
+							value={props.values.bio}
+							onInput={props.handleChange}
+						/>
+					</div>
+					<div className="gender-section section">
+						<div className="sub-content-title">Gender</div>
+						<Select
+							name="gender"
+							className="gender-selection"
+							options={[
+								{ value: "male", label: "Male" },
+								{
+									value: "female",
+									label: "Female",
+								},
+								{
+									value: "nottosay",
+									label: "Prefer not to say",
+								},
+							]}
+						/>
+						<div className="caption">
+							This won’t be part of your public profile.
+						</div>
+					</div>
+					<div className="section submit-section">
+						<Button
+							type="primary"
+							className="submit"
+							htmlType="submit"
+						>
+							Submit
+						</Button>
+						<Button>Undo</Button>
+					</div>
+				</Form>
+			)}
+		</Formik>
 	);
 }
